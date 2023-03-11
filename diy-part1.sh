ee077edf33af83987c3fa31f9a8887feecfb52f3
mrbingdian <995682961@qq.com>
2023-03-11 09:21:37 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+cp feeds.conf.default feeds.conf
+echo "src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt" >> feeds.conf
